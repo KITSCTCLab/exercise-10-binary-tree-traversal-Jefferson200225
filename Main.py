75312d2347f51200b5b50837019b30fbe03deaab
Jefferson200225 <110010433+Jefferson200225@users.noreply.github.com>
2022-10-11 12:19:26 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,11 +11,7 @@ class BinaryTreeNode:
         self.right_child = None
 
 
-def insert(root, new_value) -> BinaryTreeNode:
-    """If binary search tree is empty, make a new node, declare it as root and return the root.
-        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
-        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
-        Finally, return the root.
+
         """
     if (root == None):
         root = BinaryTreeNode(new_value)
